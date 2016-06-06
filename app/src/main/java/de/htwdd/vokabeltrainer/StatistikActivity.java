c05529d81cc75f4085079711015758ae1ee073f8
krichter1 <kai.richter3@gmx.de>
2016-06-06 08:01:04 +0200

    Erster Test der Statistiken

--- app/src/main/java/de/htwdd/vokabeltrainer/StatistikActivity.java
+++ app/src/main/java/de/htwdd/vokabeltrainer/StatistikActivity.java
@@ -1,5 +1,6 @@
 package de.htwdd.vokabeltrainer;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,7 +8,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class StatistikActivity extends AppCompatActivity {
+
+    public PieChart pc;
+    public HorizontalBarChart bc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +35,106 @@ public class StatistikActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        pc = (PieChart) findViewById(R.id.chart);
+        bc = (HorizontalBarChart) findViewById(R.id.bchart);
+        setDataPie(3, 100);
+        setDataBar(100, 100);
+
     }
+
+    private void setDataPie(int count, float range) {
+
+        float mult = range;
+
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        for (int i = 0; i < count + 1; i++) {
+            yVals1.add(new Entry((float) (Math.random() * mult) + mult / 5, i));
+        }
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < count + 1; i++)
+            xVals.add("" + i);
+
+        PieDataSet dataSet = new PieDataSet(yVals1, "Election Results");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+
+        // add a lot of colors
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        /*for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);*/
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        /*for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);*/
+
+        //colors.add(ColorTemplate.getHoloBlue());
+
+        dataSet.setColors(colors);
+        //dataSet.setSelectionShift(0f);
+
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.WHITE);
+        //data.setValueTypeface(tf);
+        pc.setData(data);
+
+        // undo all highlights
+        pc.highlightValues(null);
+
+        pc.invalidate();
+    }
+
+    private void setDataBar(int count, float range) {
+
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < count; i++) {
+            xVals.add("" + i);
+            yVals1.add(new BarEntry((float) (Math.random() * range), i));
+        }
+
+        BarDataSet set1;
+
+        if (bc.getData() != null &&
+                bc.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet)bc.getData().getDataSetByIndex(0);
+            set1.setYVals(yVals1);
+            bc.getData().setXVals(xVals);
+            bc.getData().notifyDataChanged();
+            bc.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(yVals1, "DataSet 1");
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(xVals, dataSets);
+            data.setValueTextSize(10f);
+            //data.setValueTypeface(tf);
+
+            bc.setData(data);
+        }
+    }
+
+
 
 }
